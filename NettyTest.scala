62e142e6c1b936aeb74c62809dba00a8a56318e6
José Armando García Sancio <jsancio@gmail.com>
2017-01-15 16:59:21 -0800

    Integrate reactors

--- NettyTest.scala
+++ NettyTest.scala
@@ -22,25 +22,35 @@ import io.netty.handler.codec.http.HttpVersion
 import io.netty.handler.codec.http.LastHttpContent
 import io.netty.handler.logging.LogLevel
 import io.netty.handler.logging.LoggingHandler
+import io.netty.util.ReferenceCounted
+import io.reactors.Channel
+import io.reactors.Proto
+import io.reactors.Reactor
+import io.reactors.ReactorSystem
 import java.io.InputStream
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.concurrent.locks.ReentrantLock
+import org.slf4j.LoggerFactory
+import scala.collection.immutable.Queue
 import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.Future
 import scala.util.control.NonFatal
 
+
 object NettyTest {
   def main(args: Array[String]): Unit = {
     val eventLoopGroup = new NioEventLoopGroup()
     try {
+      val system = new ReactorSystem("test-system")
+
       val bootstrap = new ServerBootstrap()
       bootstrap
         .group(eventLoopGroup)
         .channel(classOf[NioServerSocketChannel])
         .handler(new LoggingHandler(LogLevel.INFO))
-        .childHandler(NettyTestInitializer())
+        .childHandler(NettyTestInitializer(system))
 
       bootstrap.bind(8080).sync().channel().closeFuture().sync()
     } finally {
@@ -49,38 +59,121 @@ object NettyTest {
   }
 }
 
-final class NettyTestInitializer extends ChannelInitializer[SocketChannel] {
+sealed trait ChannelMessage
+case class ActiveChannelMessage(context: ChannelHandlerContext) extends ChannelMessage
+case object InactiveChannelMessage extends ChannelMessage
+
+sealed trait ReactorNettyEvent
+case class ReadChannel(channel: Channel[HttpObject]) extends ReactorNettyEvent
+
+final class ReactorTest extends Reactor[ChannelMessage] {
+  private[this] val logger = LoggerFactory.getLogger(getClass())
+  private[this] var pending = Option.empty[DynamicInputStream]
+
+  main.events.onEvent {
+    case ActiveChannelMessage(context) =>
+      logger.info(s"reactor($uid) - channel active")
+      val reply = system.channels.daemon.open[HttpObject]
+      context.pipeline().fireUserEventTriggered(ReadChannel(reply.channel))
+      reply.events.onEvent {
+        case request: HttpRequest =>
+          logger.info(s"reactor($uid) - got a htttp request message: $request")
+        case lastContent: LastHttpContent =>
+          logger.info(s"reactor($uid) - got a last http content message: $lastContent")
+          context.writeAndFlush(NettyTestHandler.createResponse())
+
+          // TODO: Release now because we have nothing to do
+          lastContent.release
+        case content: HttpContent =>
+          logger.info(s"reactor($uid) - got a http content message: $content")
+
+          // TODO: Release now because we have nothing to do
+          content.release
+      }
+
+    case InactiveChannelMessage =>
+      logger.info(s"reactor($uid) - channel inactive")
+      main.seal()
+  }
+}
+
+final class NettyTestInitializer(
+  system: ReactorSystem
+) extends ChannelInitializer[SocketChannel] {
   override def initChannel(channel: SocketChannel): Unit = {
     val pipeline = channel.pipeline()
     pipeline.addLast(new HttpResponseEncoder())
     pipeline.addLast(new HttpRequestDecoder(4096, 8192, 1))
-    pipeline.addLast(NettyTestHandler(ExecutionContext.global))
+    pipeline.addLast(NettyTestHandler(system)(ExecutionContext.global))
   }
 }
 
 object NettyTestInitializer {
-  def apply(): NettyTestInitializer = {
-    new NettyTestInitializer()
+  def apply(system: ReactorSystem): NettyTestInitializer = {
+    new NettyTestInitializer(system)
   }
 }
 
 final class NettyTestHandler(
+  system: ReactorSystem
+)(
   implicit executor: ExecutionContextExecutor
 ) extends SimpleChannelInboundHandler[HttpObject] {
-  var pending = Option.empty[DynamicInputStream]
+  private[this] val logger = LoggerFactory.getLogger(getClass())
+  private[this] var channel = Option.empty[Channel[ChannelMessage]]
+  private[this] var readChannel = Option.empty[Channel[HttpObject]]
+  private[this] var buffer = Queue.empty[HttpObject]
+
+  override def channelActive(context: ChannelHandlerContext): Unit = {
+    logger.info("handler - channel active")
+    val value = system.spawn(Proto[ReactorTest])
+    channel = Some(value)
+    value ! ActiveChannelMessage(context)
+  }
+
+  override def channelInactive(context: ChannelHandlerContext): Unit = {
+    logger.info("handler - channel inactive")
+
+    for (value <- channel) {
+      value ! InactiveChannelMessage
+    }
+  }
+
+  override def userEventTriggered(context: ChannelHandlerContext, event: AnyRef): Unit = {
+    logger.info("handler - user event triggered")
+    event match {
+      case ReadChannel(value) =>
+        readChannel = Some(value)
+        for (message <- buffer) {
+          value ! message
+        }
+        buffer = Queue.empty[HttpObject]
+    }
+  }
 
   override def channelRead0(context: ChannelHandlerContext, message: HttpObject): Unit = {
-    println(s"type of message: ${message.getClass().getName()}")
+    logger.info("handler - channel read")
+    // Retain a reference before sending or returning message
+    message match {
+      case content: ReferenceCounted =>
+        content.retain
+      case _ =>
+    }
 
+    readChannel match {
+      case Some(value) =>
+        // Send message
+        value ! message
+      case None =>
+        buffer = buffer.enqueue(message)
+    }
+  }
+
+  /*
     message match {
       case request: HttpRequest =>
-        // Got a new message start
-        println("got a new message start")
 
       case lastContent: LastHttpContent =>
-        println("got last message data")
-
-        lastContent.retain
 
         pending = pending match {
           case result @ Some(inputStream) =>
@@ -112,8 +205,6 @@ final class NettyTestHandler(
         }
 
       case content: HttpContent =>
-        println("got message data")
-
         content.retain
         pending = pending match {
           case result @ Some(inputStream) =>
@@ -146,30 +237,50 @@ final class NettyTestHandler(
         }
     }
   }
+*/
 }
 
 object NettyTestHandler {
   private val Content = Array[Byte]('H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd')
+  private[this] val logger = LoggerFactory.getLogger(getClass())
 
-  def apply(implicit executor: ExecutionContextExecutor): NettyTestHandler = {
-    new NettyTestHandler
+  def apply(
+    system: ReactorSystem
+  )(
+    implicit executor: ExecutionContextExecutor
+  ): NettyTestHandler = {
+    new NettyTestHandler(system)
+  }
+
+  def createResponse(): DefaultFullHttpResponse = {
+    val response = new DefaultFullHttpResponse(
+      HttpVersion.HTTP_1_1,
+      HttpResponseStatus.OK,
+      Unpooled.wrappedBuffer(Content)
+    )
+    response.headers().set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.TEXT_PLAIN)
+    response.headers().setInt(
+      HttpHeaderNames.CONTENT_LENGTH,
+      response.content().readableBytes()
+    )
+
+    response
   }
 
   private def copyInputStream(input: InputStream): Unit = {
     try {
-      println("copying bytes...")
+      logger.info("copying bytes...")
       Files.copy(input, Paths.get("/tmp/hello-world-netty"))
-      println("did we copy or throw?")
+      logger.info("we copied and didn't throw")
     } catch {
       case NonFatal(e) =>
-        e.printStackTrace()
+        logger.error("why an exception", e)
     } finally {
-      println("done with copy")
+      logger.info("done with copy")
     }
   }
 }
 
-// TODO: deal with retain and release
 final class DynamicInputStream(data: ByteBuf, done: Boolean) extends InputStream {
   private[this] val lock = new ReentrantLock()
   private[this] val notEmpty = lock.newCondition()
@@ -211,5 +322,6 @@ final class DynamicInputStream(data: ByteBuf, done: Boolean) extends InputStream
       lock.unlock()
     }
   }
-}
 
+  // TODO: implement close - it should release all the buffer, clear the buffer and disable add
+}
--- build.sbt
+++ build.sbt
@@ -3,9 +3,11 @@ lazy val root = (
 ).settings(
   name := "netty-test",
   version := "0.1.0",
-  scalaVersion := "2.12.1",
+  scalaVersion := "2.11.8",
   libraryDependencies ++= Seq(
     "io.netty" % "netty-codec-http" % "4.1.6.Final",
-    "io.netty" % "netty-handler" % "4.1.6.Final"
+    "io.netty" % "netty-handler" % "4.1.6.Final",
+    "io.reactors" %% "reactors" % "0.8",
+    "ch.qos.logback" % "logback-classic" % "1.1.8"
   )
 )
